1752f19d4a3c6d8793134908f75a31ad3e7bdc6b
ChuckleCharms <145775387+ChuckleCharms@users.noreply.github.com>
2025-12-10 10:08:17 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // Helps avoid allowedHosts issues in dev environments
-    allowedHosts: true,
-  },
 });
+
